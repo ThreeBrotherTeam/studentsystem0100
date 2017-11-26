e468a4fd0811d597e5485fbccc34cca7fe48c419
guowangyunyan <924631391@qq.com>
2017-11-26 15:14:41 +0800

    登录功能

---
+++ src/com/training/controller/StudentController.java
@@ -0,0 +1,36 @@
+package com.training.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.training.common.config.Config;
+import com.training.model.data.StudentData;
+import com.training.model.form.StudentForm;
+import com.training.page.Pagination;
+import com.training.page.SearchResult;
+import com.training.service.StudentService;
+
+@Controller
+public class StudentController {
+
+	@Resource
+	private StudentService studentService;
+
+	@RequestMapping("/loadStudentsByFields")
+	public String loadStudentsByFields(Model model, StudentForm studentForm,
+			@RequestParam(value = "currentPage", defaultValue = "1") int currentPage) {
+		Pagination page = new Pagination();
+		if (currentPage <= 0) {
+			currentPage = Integer.parseInt(Config.getStringProperty("page.currentPage"));
+		}
+		page.setCurrentPage(currentPage);
+		page.setPageSize(Integer.parseInt(Config.getStringProperty("page.pageSize")));
+		SearchResult<StudentData> searchResult = studentService.findAll(studentForm, page);
+		model.addAttribute("searchResult", searchResult);
+		return "users/students";
+	}
+}
---
+++ src/com/training/controller/VerifyCodeController.java
@@ -0,0 +1,24 @@
+package com.training.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.training.util.VerifyCodeUtils;
+
+@Controller
+public class VerifyCodeController {
+	@RequestMapping("/verifyCode")
+	@ResponseBody
+	public String verifyCode(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String verifyCode = VerifyCodeUtils.generateVerifyCode(4);
+		System.out.println(verifyCode);
+		request.getSession().setAttribute("verifyCode", verifyCode);
+		return "VerifyCodeUtils.outputImage(140, 50, response.getOutputStream(), verifyCode)";
+	}
+}
